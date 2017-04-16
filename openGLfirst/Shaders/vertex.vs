d77f52edc812d804d0bad3914cb6337387030767
kolarus <road2ps@gmail.com>
2017-04-16 09:28:13 +0300

    Press keyUP or keyDOWN to adjust mix value
    KeyUP == increase, keyDOWN == DECREASE

--- openGLfirst/Shaders/vertex.vs
+++ openGLfirst/Shaders/vertex.vs
@@ -1,13 +1,15 @@
 #version 330 core
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texCoord;
 
 out vec3 ourColor;
+out vec2 TexCoord;
 
-uniform vec4 ourXPosition;
 
 void main()
 {
-    gl_Position = vec4(ourXPosition,position.y, position.z, 1.0f);
+    gl_Position = vec4(position, 1.0f);
     ourColor = color;
+    TexCoord = vec2(texCoord.x+1,1-texCoord.y);
 }
--- openGLfirst/Source.cpp
+++ openGLfirst/Source.cpp
@@ -9,14 +9,16 @@
 #include "glfw.h"
 #include "errHandler.h"
 #include "Shader.h"
+#include <SOIL.h>
+#include <GL\GL.h>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-	// Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
-	// и приложение после этого закроется
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-}
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+
+GLfloat mixValue = 0.2f;
 
 int main(int argc, char* argv[])
 {
@@ -69,74 +71,74 @@ int main(int argc, char* argv[])
 	-------------------------------------------------------------*/
 
 	Shader ourShader("Shaders/vertex.vs", "Shaders/fragmenr.frag");
-	/*const GLchar* vertexShaderSource = "#version 330 core \n"
-		"layout(location = 0) in vec3 position;\n"
-		"layout(location = 1) in vec3 color;\n"
-		"out vec3 ourColor;\n"
-		"void main(){ \n"
-		"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-		"ourColor = color;\n"
-		"}\n";
-	//creating shader object
-	GLuint vertexShader;
-	// writing shader into object,GL_VERTEX_SHADER - shader type
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// binding shader with its source code, 1 - number of lines
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	// handling shader error
-	hndl.handleShaderError(vertexShader);
 
-	const GLchar* fragmentShaderSource = "#version 330 core\n"
-		"in vec3 ourColor;\n"
-		"out vec4 color;\n"
-		"void main(){\n"
-		"color = vec4(ourColor, 1.0f);\n"
-		"};\n";
-	GLuint fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	// handling shader error
-	hndl.handleShaderError(fragmentShader);
-
-	//Creating  shader program
-	GLuint shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glUseProgram(shaderProgram);
-	//Shaders are no longer needed after creating shader program
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);*/
+	
 
 	GLfloat vertices[] = {
-		// Позиции         // Цвета
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Нижний правый угол
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Нижний левый угол
-		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Верхний угол
+		// Позиции          // Цвета             // Текстурные координаты
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
 	};
-	GLuint indices[] = {  // Помните, что мы начинаем с 0!
-		0, 1, 2
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3, // First Triangle
+		1, 2, 3  // Second Triangle
 	};
-	GLuint VBO, VAO;
+	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	// TexCoord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0); // Unbind VAO
+
+
+
+
+	int twidth[2], theight[2];
+	unsigned char* image1 = SOIL_load_image("images/container.jpg", &twidth[0], &theight[0], 0, SOIL_LOAD_RGB);
+	unsigned char* image2 = SOIL_load_image("images/awesomeface.png", &twidth[1], &theight[1], 0, SOIL_LOAD_RGB);
+	GLuint texture1, texture2;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);  // NOTE the GL_NEAREST Here! 
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);  // NOTE the GL_NEAREST Here!
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, twidth[0], theight[0], 0, GL_RGB, GL_UNSIGNED_BYTE, image1);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image1);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);  // NOTE the GL_NEAREST Here! 
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);  // NOTE the GL_NEAREST Here!
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, twidth[1], theight[1], 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image2);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
 
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -150,17 +152,26 @@ int main(int argc, char* argv[])
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw the triangle
 
-		glUniform3f(glGetUniformLocation(ourShader.Program, "ourPosition"), 0.7f,0.7f,0.7f);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"), 1);
 
+		glUniform1f(glGetUniformLocation(ourShader.Program, "mixValue"), mixValue);
+		// Activate shader
 		ourShader.Use();
+
+		// Draw container
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
+
 	}
 	// Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
@@ -168,4 +179,23 @@ int main(int argc, char* argv[])
 	// cleaninng resources before quit
 	glfwTerminate();
 	return 0;
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
+	// и приложение после этого закроется
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+	{
+		mixValue += 0.1f;
+		if (mixValue >= 1.0f)
+			mixValue = 1.0f;
+	}
+	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+	{
+		mixValue -= 0.1f;
+		if (mixValue <= 0.0f)
+			mixValue = 0.0f;
+	}
 }
